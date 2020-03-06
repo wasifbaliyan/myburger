e5d001f4b82311814d0ac40028943ab3675f0eab
Wasif Baliyan <w.baliyan@gmail.com>
2020-03-06 12:17:54 +0530

    Updates

---
+++ precache-manifest.e36688f604376300c1d706a82942baf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e6eb51480d8e988c678769fb126967c",
+    "url": "/myburger/index.html"
+  },
+  {
+    "revision": "a8cbe371e944f83ed785",
+    "url": "/myburger/static/css/main.f7253a78.chunk.css"
+  },
+  {
+    "revision": "2ddeb2832215c56f2765",
+    "url": "/myburger/static/js/2.44d5fdf7.chunk.js"
+  },
+  {
+    "revision": "1848cdebbd5f1da5759e0ff3f79534a9",
+    "url": "/myburger/static/js/2.44d5fdf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8cbe371e944f83ed785",
+    "url": "/myburger/static/js/main.3bc27419.chunk.js"
+  },
+  {
+    "revision": "1634223f1b4b9e1419c5",
+    "url": "/myburger/static/js/runtime-main.ec55d731.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/myburger/static/media/burger-logo.b8503d26.png"
+  }
+]);
